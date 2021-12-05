71f4a94797e2f761ed024687aada0ae825771cff
Brett Rehberg <brett.rehberg@gmail.com>
2021-12-04 22:17:19 -0600

    decompose data processing function

--- day04/src/processBingoData.js
+++ day04/src/processBingoData.js
@@ -1,35 +1,13 @@
 function processBingoData(input, winner = "first") {
-  const possibleLines = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
-    [0, 5, 10, 15, 20],
-    [1, 6, 11, 16, 21],
-    [2, 7, 12, 17, 22],
-    [3, 8, 13, 18, 23],
-    [4, 9, 14, 19, 24],
-  ];
-
-  const bingoBoards = [];
-  input.boards.forEach((rawBoard) => {
-    bingoBoards.push({
-      flatValues: rawBoard.flat(),
-      markedValues: Array(25).fill(false),
-    });
-  });
+  const possibleWinningLines = initializeWinningLines();
+  const bingoBoards = parseInputBoards(input.boards);
 
   for (const drawnNumber of input.numbers) {
-    // mark all bingo boards as each number is drawn
-    bingoBoards.forEach((bingoBoard) => {
-      const position = bingoBoard.flatValues.indexOf(drawnNumber);
-      if (position !== -1) bingoBoard.markedValues[position] = true;
-    });
+    markAllBingoBoards(drawnNumber);
 
     // check each board to see if any winning line has been marked
     for (const [boardIndex, currentBoard] of bingoBoards.entries()) {
-      for (const thisLine of possibleLines) {
+      for (const thisLine of possibleWinningLines) {
         // validate total marked values for this line (5 is a winner)
         const checkWinningLine = currentBoard.markedValues.filter(
           (flag, position) => flag && thisLine.includes(position)
@@ -40,15 +18,50 @@ function processBingoData(input, winner = "first") {
         // otherwise remove the current winning board from consideration
         if (winner === "first" || bingoBoards.length === 1) {
           const sumOfUnmarkedValues = currentBoard.flatValues
-            .filter((value, index) => !currentBoard.markedValues[index])
+            .filter((_value, index) => !currentBoard.markedValues[index])
             .reduce((partialSum, value) => partialSum + value, 0);
           return drawnNumber * sumOfUnmarkedValues;
         } else {
           bingoBoards.splice(boardIndex, 1);
+          break;
         }
       }
     }
   }
   return -1; // no winner was found
+
+  function markAllBingoBoards(drawnNumber) {
+    // mark all bingo boards as each number is drawn
+    bingoBoards.forEach((bingoBoard) => {
+      const position = bingoBoard.flatValues.indexOf(drawnNumber);
+      if (position !== -1) bingoBoard.markedValues[position] = true;
+    });
+  }
+
+  function parseInputBoards(inputBoards) {
+    const bingoBoards = [];
+    inputBoards.forEach((rawBoard) => {
+      bingoBoards.push({
+        flatValues: rawBoard.flat(),
+        markedValues: Array(25).fill(false),
+      });
+    });
+    return bingoBoards;
+  }
+
+  function initializeWinningLines() {
+    return [
+      [0, 1, 2, 3, 4],
+      [5, 6, 7, 8, 9],
+      [10, 11, 12, 13, 14],
+      [15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24],
+      [0, 5, 10, 15, 20],
+      [1, 6, 11, 16, 21],
+      [2, 7, 12, 17, 22],
+      [3, 8, 13, 18, 23],
+      [4, 9, 14, 19, 24],
+    ];
+  }
 }
 exports.processBingoData = processBingoData;
