12e47e987590e93c5c950a60c2dd46f854ddb641
Brett Rehberg <brett.rehberg@gmail.com>
2021-12-10 09:47:32 -0600

    extract functions into symbol stack object

--- day10/src/processSyntaxChunks.js
+++ day10/src/processSyntaxChunks.js
@@ -1,49 +1,58 @@
-const openSymbols = ["(", "[", "{", "<"];
-const closeSymbols = [")", "]", "}", ">"];
-const symbolScores = [3, 57, 1197, 25137];
-
 function processSyntaxChunks(syntaxInput, handleIncomplete = false) {
-  let totalSyntaxScore = 0;
-  const completionScores = [];
-
+  const symbolStack = initializeSymbolStack();
   for (const currentLine of syntaxInput) {
-    let currentLineScore = 0;
-    const symbolStack = [];
+    let incompleteLine = symbolStack.startNewLine();
     for (const character of currentLine) {
-      currentLineScore = handleSymbol(character, symbolStack);
-      if (currentLineScore) {
-        totalSyntaxScore += currentLineScore;
-        break; // current line is corrupt, halt!
-      }
+      incompleteLine = symbolStack.handleSymbol(character);
+      if (!incompleteLine) break; // current line is corrupt, halt!
     }
-    if (handleIncomplete && !currentLineScore)
-      completionScores.push(completeLineScore(symbolStack));
+    if (handleIncomplete && incompleteLine)
+      symbolStack.computeLineCompletionScore();
   }
-
-  const completionScore = middleScore(completionScores);
-  return handleIncomplete ? completionScore : totalSyntaxScore;
+  return handleIncomplete
+    ? symbolStack.completionScore()
+    : symbolStack.syntaxScore();
 }
 exports.processSyntaxChunks = processSyntaxChunks;
 
-function handleSymbol(symbol, stack) {
-  if (openSymbols.includes(symbol)) {
-    stack.push(symbol);
-  } else if (closeSymbols.includes(symbol)) {
-    const symbolIndex = closeSymbols.indexOf(symbol);
-    const lastSymbol = stack.pop();
-    if (lastSymbol !== openSymbols[symbolIndex]) {
-      return symbolScores[symbolIndex];
-    }
-  }
-}
+function initializeSymbolStack() {
+  const openSymbols = ["(", "[", "{", "<"];
+  const closeSymbols = [")", "]", "}", ">"];
+  const symbolScores = [3, 57, 1197, 25137];
 
-function completeLineScore(stack) {
-  return stack.reduceRight((total, symbol) => {
-    return total * 5 + openSymbols.indexOf(symbol) + 1;
-  }, 0);
-}
-
-function middleScore(scores) {
-  const middle = parseInt(scores.length / 2);
-  return scores.sort((a, b) => a - b)[middle];
+  let [totalSyntaxScore, stack] = [0, []];
+  const completionScores = [];
+  return {
+    startNewLine: function () {
+      stack = [];
+      return true;
+    },
+    handleSymbol: function (symbol) {
+      let corruptedLine = false;
+      if (openSymbols.includes(symbol)) {
+        stack.push(symbol);
+      } else if (closeSymbols.includes(symbol)) {
+        const symbolIndex = closeSymbols.indexOf(symbol);
+        const lastSymbol = stack.pop();
+        if (lastSymbol !== openSymbols[symbolIndex]) {
+          totalSyntaxScore += symbolScores[symbolIndex];
+          corruptedLine = true;
+        }
+      }
+      return !corruptedLine;
+    },
+    computeLineCompletionScore: function () {
+      const score = stack.reduceRight((total, symbol) => {
+        return total * 5 + openSymbols.indexOf(symbol) + 1;
+      }, 0);
+      completionScores.push(score);
+    },
+    completionScore: function () {
+      const middle = parseInt(completionScores.length / 2);
+      return completionScores.sort((a, b) => a - b)[middle];
+    },
+    syntaxScore: function () {
+      return totalSyntaxScore;
+    },
+  };
 }
