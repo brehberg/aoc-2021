53df2be75743ecb6cbd2241e489f82fcdbf102b1
Brett Rehberg <brett.rehberg@gmail.com>
2021-12-12 14:36:22 -0600

    replace iterative BFS with recursive DFS

--- day12/src/processCaveSystem.js
+++ day12/src/processCaveSystem.js
@@ -1,34 +1,32 @@
 function processCaveSystem(graphEdges, allowSmall = false) {
+  const [graphStart, graphEnd] = ["start", "end"];
   const fullGraph = [];
-  for (const edge of graphEdges) {
-    const [startNode, endNode] = [...edge];
-    if (startNode !== "end" && endNode !== "start") {
+  for (const [startNode, endNode] of graphEdges) {
+    if (startNode !== graphEnd && endNode !== graphStart) {
       fullGraph.push({ start: startNode, end: endNode });
     }
-    if (startNode !== "start" && endNode !== "end") {
+    if (startNode !== graphStart && endNode !== graphEnd) {
       fullGraph.push({ start: endNode, end: startNode });
     }
   }
 
   const finishedPaths = [];
-  const inProgressPaths = [["start"]];
-  while (inProgressPaths.length) {
-    const potentialPath = inProgressPaths.shift();
-    const lastNode = potentialPath[potentialPath.length - 1];
+  findPathsToEnd([graphStart], graphStart, allowSmall);
+  return finishedPaths.length;
+
+  function findPathsToEnd(potentialPath, lastNode, allowSmall) {
     const visited = potentialPath.filter((str) => str !== str.toUpperCase());
     const firstVisit = allowSmall && new Set(visited).size === visited.length;
 
     for (const edge of fullGraph) {
       if (edge.start === lastNode) {
         if (!firstVisit && visited.includes(edge.end)) continue;
-        if (edge.end === "end") {
-          finishedPaths.push([...potentialPath, edge.end]);
-        } else {
-          inProgressPaths.push([...potentialPath, edge.end]);
-        }
+        let newPath = [...potentialPath, edge.end];
+        edge.end === graphEnd
+          ? finishedPaths.push(newPath)
+          : findPathsToEnd(newPath, edge.end, allowSmall);
       }
     }
   }
-  return finishedPaths.length;
 }
 exports.processCaveSystem = processCaveSystem;
