1a981d93b9a797489b5a1b77c2dea6e8fbe99306
Brett Rehberg <brett.rehberg@gmail.com>
2021-12-15 17:57:01 -0600

    extract logic to use arrow functions

--- day14/src/processPolymerFormula.js
+++ day14/src/processPolymerFormula.js
@@ -1,43 +1,55 @@
-function processPolymerFormula(inputFormula, maxStep = 4) {
+function processPolymerFormula(formula, maxStep = 4) {
+  let pairs = initializeFormulaPairs(formula.template);
+  for (let step = 0; step < maxStep; step++)
+    pairs = applyInsertionRules(pairs, formula.rules);
+  const result = collectFinalResults(pairs, formula.template);
+  return calculateElementScore(result);
+}
+exports.processPolymerFormula = processPolymerFormula;
+
+const initializeFormulaPairs = (template) => {
   // start with all pairs from the polymer template
-  let pairs = new PolymerPairs();
-  for (const [index, char] of inputFormula.template.entries()) {
-    if (index === inputFormula.template.length - 1) break;
-    const pair = char + inputFormula.template[index + 1];
+  const pairs = new PolymerPairs();
+  for (const [index, char] of template.entries()) {
+    if (index === template.length - 1) break;
+    const pair = char + template[index + 1];
     pairs.addPair(pair);
   }
+  return pairs;
+};
+const applyInsertionRules = (pairs, rules) => {
   // for each step determine new pairs based on insertion rules
-  for (let step = 0; step < maxStep; step++) {
-    const newPairs = new PolymerPairs();
-    for (const pair in pairs.pairList) {
-      const rule = inputFormula.rules.find((rule) => rule[0] === pair);
-      const pair1 = rule[0][0] + rule[1];
-      const pair2 = rule[1] + rule[0][1];
-      const count = pairs.pairList[pair];
-      newPairs.addPair(pair1, count);
-      newPairs.addPair(pair2, count);
-    }
-    pairs = newPairs;
+  const newPairs = new PolymerPairs();
+  for (const pair in pairs.pairList) {
+    const rule = rules.find((rule) => rule[0] === pair);
+    const pair1 = rule[0][0] + rule[1];
+    const pair2 = rule[1] + rule[0][1];
+    const count = pairs.pairList[pair];
+    newPairs.addPair(pair1, count);
+    newPairs.addPair(pair2, count);
   }
+  return newPairs;
+};
+const collectFinalResults = (pairs, template) => {
   // count second char from all final pairs plus first template char
   const result = new PolymerPairs();
-  result.addPair(inputFormula.template[0], 1);
+  result.addPair(template[0]);
   for (const pair in pairs.pairList) {
     const count = pairs.pairList[pair];
     result.addPair(pair[1], count);
   }
+  return result;
+};
+const calculateElementScore = (result) => {
   // subtract least common quantity from most common quantity
-  let most = 0;
-  let least = Infinity;
+  let [most, least] = [0, Infinity];
   for (const element in result.pairList) {
     const count = result.pairList[element];
     most = Math.max(most, count);
     least = Math.min(least, count);
   }
   return most - least;
-}
-exports.processPolymerFormula = processPolymerFormula;
-
+};
 class PolymerPairs {
   pairList = {};
   addPair(pair, count = 1) {
