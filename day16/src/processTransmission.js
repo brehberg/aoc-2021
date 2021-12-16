9395c90b7ee08bdc302b93e119916b3c3bb46340
Brett Rehberg <brett.rehberg@gmail.com>
2021-12-16 17:35:11 -0600

    extract parsing logic to arrow functions

--- day16/src/processTransmission.js
+++ day16/src/processTransmission.js
@@ -4,71 +4,89 @@ function processTransmission(hexString) {
     .map((str) => parseInt(str, 16).toString(2).padStart(4, "0"))
     .join("");
 
-  const versions = [];
-  let number = 0;
-  parsePacket(bitString);
+  const [versions, number, _] = parsePacket(bitString);
   return { version: versions.reduce((s, v) => s + v, 0), value: number };
-
-  function parsePacket(input) {
-    let binary = new BitString(input);
-    const vers = binary.getNumber(3);
-    const type = binary.getNumber(3);
-    versions.push(vers);
-    let flag = binary.getNumber(1);
-
-    // decode literal value packet
-    if (type === 4) {
-      let digits = "";
-      while (flag === 1) {
-        digits += binary.getText(4);
-        flag = binary.getNumber(1);
-      }
-      digits += binary.getText(4);
-      number = parseInt(digits, 2);
-      return binary.str;
-    }
-
-    // decode operator packets
-    let results = [];
-    if (flag === 0) {
-      // handle total length of the sub-packets
-      const length = binary.getNumber(15);
-      let subpacketText = binary.getText(length);
-      while (subpacketText.length) {
-        subpacketText = parsePacket(subpacketText);
-        results.push(number);
-      }
-    } else {
-      // handle number of sub-packets contained
-      const count = binary.getNumber(11);
-      let remainingText = binary.str;
-      for (let i = 0; i < count; i++) {
-        remainingText = parsePacket(remainingText);
-        results.push(number);
-      }
-      binary = new BitString(remainingText);
-    }
-    // compute value of operator expression
-    if (type === 0) {
-      number = results.reduce((sum, v) => sum + v, 0);
-    } else if (type === 1) {
-      number = results.reduce((prod, v) => prod * v, 1);
-    } else if (type === 2) {
-      number = Math.min(...results);
-    } else if (type === 3) {
-      number = Math.max(...results);
-    } else if (type === 5) {
-      number = Number(results[0] > results[1]);
-    } else if (type === 6) {
-      number = Number(results[0] < results[1]);
-    } else if (type === 7) {
-      number = Number(results[0] === results[1]);
-    }
-    return binary.str;
-  }
 }
 exports.processTransmission = processTransmission;
 
+const parsePacket = (input) => {
+  const binary = new BitString(input);
+  const vers = binary.getNumber(3);
+  const type = binary.getNumber(3);
+
+  if (type === 4) {
+    return decodeLiteral(binary, vers);
+  }
+  const [versions, results] = decodeOperator(binary, vers);
+  return [versions, computeResults(type, results), binary.remaining()];
+};
+
+const decodeLiteral = (binary, vers) => {
+  // decode literal value packet
+  let flag = binary.getNumber(1);
+  let digits = binary.getText(4);
+  while (flag) {
+    flag = binary.getNumber(1);
+    digits += binary.getText(4);
+  }
+  return [[vers], parseInt(digits, 2), binary.remaining()];
+};
+
+const decodeOperator = (binary, vers) => {
+  // decode operator packet and subpackets
+  const flag = binary.getNumber(1);
+  const [versions, results] = [[vers], []];
+  flag === 0
+    ? subpacketsByLength(binary, versions, results)
+    : subpacketsByCount(binary, versions, results);
+  return [versions, results];
+};
+
+const computeResults = (type, results) => {
+  // compute value of operator expression
+  let result = 0;
+  if (type === 0) {
+    result = results.reduce((sum, v) => sum + v, 0);
+  } else if (type === 1) {
+    result = results.reduce((prod, v) => prod * v, 1);
+  } else if (type === 2) {
+    result = Math.min(...results);
+  } else if (type === 3) {
+    result = Math.max(...results);
+  } else if (type === 5) {
+    result = Number(results[0] > results[1]);
+  } else if (type === 6) {
+    result = Number(results[0] < results[1]);
+  } else if (type === 7) {
+    result = Number(results[0] === results[1]);
+  }
+  return result;
+};
+
+const subpacketsByLength = (binary, versions, results) => {
+  // handle total length of the sub-packets
+  let [subversions, number] = [[], 0];
+  const length = binary.getNumber(15);
+  let subpacketText = binary.getText(length);
+  while (subpacketText.length) {
+    [subversions, number, subpacketText] = parsePacket(subpacketText);
+    versions.push(...subversions);
+    results.push(number);
+  }
+};
+
+const subpacketsByCount = (binary, versions, results) => {
+  // handle number of sub-packets contained
+  let [subversions, number] = [[], 0];
+  const count = binary.getNumber(11);
+  let remainingText = binary.remaining();
+  for (let i = 0; i < count; i++) {
+    [subversions, number, remainingText] = parsePacket(remainingText);
+    versions.push(...subversions);
+    results.push(number);
+  }
+  binary.update(remainingText);
+};
 class BitString {
   constructor(input) {
     this.str = input;
@@ -80,5 +98,11 @@ class BitString {
     const value = this.str.substring(0, len);
     this.str = this.str.slice(len);
     return value;
+  }
+  remaining() {
+    return this.str;
+  }
+  update(newString) {
+    this.str = newString;
   }
 }
